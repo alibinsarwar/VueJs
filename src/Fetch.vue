114e0603545e2b396d53f1599adbe550e25a45ed
Nobita <alibinsarwar2001@gmail.com>
2022-08-30 14:05:25 +0500

    uploading files

---
+++ src/Fetch.vue
@@ -0,0 +1,7 @@
+<script setup>
+import FetchApi from './components/FetchApi.vue'
+</script>
+
+<template>
+    <FetchApi></FetchApi>
+</template>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import Fetch from './Fetch.vue'
+
+createApp(App).mount('#app')
+createApp(Fetch).mount('#Fetch')
+
+
+
