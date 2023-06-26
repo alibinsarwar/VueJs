513fa866f7a7bcbf925ed145943b4a9c42d6a649
Muhammad Ali <alibinsarwar2001@gmail.com>
2023-06-26 16:20:17 +0500

    add dist

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/VueJs/',
   plugins: [vue()],
-  publicPath: process.env.NODE_ENV === "production" ? "/VueJs/" : "/",
 })
